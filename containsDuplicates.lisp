28af7cc05d35c11323fab6e6457254fbb933a344
charje <charles.b.jackson@protonmail.com>
2019-10-26 23:51:51 -0500

    Solved

---
+++ containsDuplicates.lisp
@@ -0,0 +1,18 @@
+;; for whatever reason this solution got stack overflow
+(defun containsDuplicates (a &key (*track* (make-hash-table)))
+  (cond
+   ((not a) nil)
+   ((gethash (car a) *track*) t)
+   (t (progn
+        (setf (gethash (car a) *track*) t)
+        (containsDuplicates (cdr a) :*track* *track*)))))
+;; and this one worked just fine
+(defun containsDuplicates (a)
+    (let ((*track* (make-hash-table))
+          (lst (coerce a 'list)))
+         (loop for n in lst
+          do
+          (if (gethash n *track*) 
+              (return-from containsDuplicates t)
+            (setf (gethash n *track*) t)))
+          (return-from containsDuplicates nil)))
